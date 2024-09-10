81f958a10cb8b7c16efacbbcbea869c77737ba3a
Ananias <netim.cunha@gmail.com>
2024-09-10 19:12:36 -0300

    conclusão do projeto ,side bar completa

--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,11 +1,28 @@
-export default function Suggestions() {
-  const suggestion=[
-    {imagemUsuario:"assets/img/meowed.svg",nomeUsuario:"meowed",conteudo:"assets/img/gato-telefone.svg",numeroCurtidas:101.523},
-    {imagemUsuario:"assets/img/barked.svg" ,nomeUsuario:"barked" ,conteudo:"assets/img/dog.svg" ,numeroCurtidas:101.525},
-    {imagemUsuario:"assets/img/chibirdart.svg" ,nomeUsuario:"chibirdart" ,conteudo:"assets/img/adorable_animals.svg" ,numeroCurtidas:101.445}
+export default function Suggestion() {
+  const sugestoes = [
+    { imagemUsuario: "assets/img/meowed.svg", nomeUsuario: "meowed", razao: "seguiu você" },
+    { imagemUsuario: "assets/img/barked.svg", nomeUsuario: "barked", razao: "novo no Instagram" },
+    { imagemUsuario: "assets/img/chibirdart.svg", nomeUsuario: "chibirdart", razao: "seguiu você" }
+  ];
 
-    ];
-    return (
-    <h1>adsjaskjdsa</h1>
-    );
-  }
+  return (
+    <div className="sugestoes">
+      <div className="titulo">
+        Sugestões para você
+        <div>Ver tudo</div>
+      </div>
+      {sugestoes.map((sugestao) => (
+        <div className="sugestao">
+          <div className="usuario">
+            <img src={sugestao.imagemUsuario} alt="" />
+            <div className="texto">
+              <div className="nome">{sugestao.nomeUsuario}</div>
+              <div className="razao">{sugestao.razao}</div>
+            </div>
+          </div>
+          <div className="seguir">Seguir</div>
+        </div>
+      ))}
+    </div>
+  );
+}
