835559c96c1549dc703fb9e1939446dffa8fe2bf
Ananias <netim.cunha@gmail.com>
2024-09-09 23:23:13 -0300

    User completo ,com funções de mudança de foto e nome de usuario

--- src/User.js
+++ src/User.js
@@ -1,5 +1,35 @@
+import React from 'react'
+
+
 export default function User() {
+ const [nome,setNome]=React.useState("catanacomics");
+ const [imagemUser,setImagemUser]=React.useState("assets/img/catanacomics.svg");
+ function mudaNome(){
+  let novoNome=prompt("digite novo nome");
+  if (novoNome){
+    setNome(novoNome);
+  }
+ }
+
+
+ function mudaImg(){
+  let novaImg=prompt("digite novo nome");
+  if (novaImg){
+    setImagemUser(novaImg);
+  }
+ }
+
+
     return (
-    <h1>adsjaskjdsa</h1>
+      <div class="usuario">
+          <img onClick={mudaImg} src={imagemUser} alt="imagem de perfil"/>
+          <div class="texto">
+            <span>
+              <strong>{nome}</strong>
+              <ion-icon onClick={mudaNome} name="pencil"></ion-icon>
+            </span>
+          </div>
+        </div>
     );
   }
+
